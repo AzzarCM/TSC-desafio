cc3e5b22741bd2bfd2d3d3821eb72189f87970e6
Cristian Mundo <00086917@uca.edu.sv>
2020-06-22 17:00:26 -0600

    just setting everything up

---
+++ src/sample/controllers/Controller.java
@@ -0,0 +1,23 @@
+package sample.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+
+public class Controller{
+
+
+    public void changeScreen(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent screenParent = FXMLLoader.load(getClass().getResource("/sample/views/model.fxml"));
+        Scene tableViewScene = new Scene(screenParent);
+
+        //obtenemos la informacion de estado
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(tableViewScene);
+        window.show();
+    }
+}
---
+++ src/sample/controllers/Model.java
@@ -0,0 +1,33 @@
+package sample.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Model {
+
+    public void backTo(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent screenParent = FXMLLoader.load(getClass().getResource("/sample/views/sample.fxml"));
+        Scene tableViewScene = new Scene(screenParent);
+
+        //obtenemos la informacion de estado
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    public void goForward(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent screenParent = FXMLLoader.load(getClass().getResource("/sample/views/sample.fxml"));
+        Scene tableViewScene = new Scene(screenParent);
+
+        //obtenemos la informacion de estado
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(tableViewScene);
+        window.show();
+    }
+
+}
